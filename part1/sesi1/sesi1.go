27f8d2453b668d9dbb8e6b41c960a92158ccb8d4
maskimmm <rifqi231@gmail.com>
2023-03-10 10:21:23 +0700

    Assignment 1

--- part1/sesi1/sesi1.go
+++ part1/sesi1/sesi1.go
@@ -7,16 +7,20 @@ func main() {
 	var j bool = true
 	var k float64 = 123.456
 	ja := 'Я'
+	jaU := rune(1071)
 	fmt.Printf("%v\n", i)
 	fmt.Printf("%T\n", i)
 	fmt.Println("%")
-	fmt.Println(j)
+	fmt.Printf("%t\n\n", j)
+
 	fmt.Printf("%b\n", i)
+	fmt.Printf("%c\n", ja)
 	fmt.Printf("%d\n", i)
 	fmt.Printf("%o\n", i)
 	fmt.Printf("%x\n", 15)
-	fmt.Printf("%U\n", ja)
+	fmt.Printf("%X\n", 15)
+	fmt.Printf("%U\n\n", jaU)
+
 	fmt.Printf("%f\n", k)
 	fmt.Printf("%E\n", k)
-
 }
